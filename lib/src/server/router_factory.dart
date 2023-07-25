0bdcdce213ce2638e836fe7c4f6c75e8f30ab589
Justin Shepertycki <justin.shepertycki@gmail.com>
2023-07-24 21:46:58 -0500

    Fix router base path routing (#17)

--- lib/src/server/router_factory.dart
+++ lib/src/server/router_factory.dart
@@ -5,13 +5,13 @@ import 'package:shelf_router/shelf_router.dart';
 abstract final class RouterFactory {
   /// Creates a Shelf [Router] from a list of [ApiMixin] apis. If [apiVersionPrefix] is
   /// provided, it will be prepended to all routes.
-  static Router create(List<ApiMixin> apis, {String apiVersionPrefix = ''}) {
+  static Router create(List<ApiMixin> apis, {String routerBasePath = ''}) {
     final router = Router();
 
     for (final api in apis) {
       for (final route in api.routes) {
         router.add(route.verb.value,
-            '$apiVersionPrefix${api.prefix}${route.path}', route.handler);
+            '$routerBasePath${api.prefix}${route.path}', route.handler);
       }
     }
     return router;
